4adb041b913e4519e5d77bdc50607941500cd02b
Ngoc Ho <phuongngoc31@gmail.com>
2025-07-20 10:48:35 +0700

    Minor changes for slides and labs material
    Signed-off-by: Ngoc Ho <phuongngoc31@gmail.com>

--- mk/embedded-linux-bbb-simple-project.mk
+++ mk/embedded-linux-bbb-simple-project.mk
@@ -1,6 +1,9 @@
 EMBEDDED_LINUX_BBB_SIMPLE_PROJECT_SLIDES = \
     first-slide \
-    linux-intro
+    linux-intro \
+    linux-dev-environment \
+    linux-toolchains \
+    linux-toolchains-definition
 
 EMBEDDED_LINUX_BBB_SIMPLE_PROJECT_LABS = \
     setup \
