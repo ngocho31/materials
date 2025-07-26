7a489f5429a4c3c088ccca103e07e089942bef7d
Ngoc Ho <phuongngoc31@gmail.com>
2025-07-26 23:50:55 +0700

    Add linux bootloaders slides
    Signed-off-by: Ngoc Ho <phuongngoc31@gmail.com>

--- mk/embedded-linux-bbb-simple-project.mk
+++ mk/embedded-linux-bbb-simple-project.mk
@@ -5,7 +5,11 @@ EMBEDDED_LINUX_BBB_SIMPLE_PROJECT_SLIDES = \
     linux-toolchains \
     linux-toolchains-definition \
     linux-toolchains-options \
-    linux-toolchains-obtaining
+    linux-toolchains-obtaining \
+    linux-bootloaders \
+    linux-bootloaders-sequence \
+    linux-bootloaders-u-boot \
+    linux-bootloaders-tf-a
 
 EMBEDDED_LINUX_BBB_SIMPLE_PROJECT_LABS = \
     setup \
