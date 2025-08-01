f119d2d19f0fed358973e41aa698f3b01d14c1c3
Ngoc <v.ngochp3@vinfast.vn>
2025-08-01 16:53:33 +0700

    Create embedded linux slide
    Signed-off-by: Ngoc <v.ngochp3@vinfast.vn>

--- mk/embedded-linux-bbb-simple-project.mk
+++ mk/embedded-linux-bbb-simple-project.mk
@@ -1,29 +1,3 @@
-EMBEDDED_LINUX_BBB_SIMPLE_PROJECT_SLIDES = \
-    first-slide \
-    beagleboneblack-board \
-    linux-intro \
-    linux-dev-environment \
-    linux-toolchains \
-    linux-toolchains-definition \
-    linux-toolchains-options \
-    linux-toolchains-obtaining \
-    linux-bootloaders \
-    linux-bootloaders-sequence \
-    linux-bootloaders-u-boot \
-    linux-bootloaders-tf-a \
-    linux-kernel-intro \
-    linux-kernel-intro-features \
-    linux-kernel-intro-configuration \
-    linux-kernel-intro-building \
-    linux-kernel-intro-booting \
-    linux-kernel-root-filesystem \
-    linux-kernel-root-filesystem-principles \
-    linux-kernel-root-filesystem-initramfs \
-    linux-kernel-root-filesystem-contents \
-    linux-kernel-root-filesystem-virtual-fs \
-    linux-kernel-root-filesystem-minimal \
-    linux-kernel-root-filesystem-boot-sequence
-
 EMBEDDED_LINUX_BBB_SIMPLE_PROJECT_LABS = \
     setup \
     linux-toolchain
