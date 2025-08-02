2a5a5825e1f806e712b9afef16107b5616e4af64
Ngoc <v.ngochp3@vinfast.vn>
2025-08-02 16:57:20 +0700

    Draft

---
+++ mk/embedded-linux-drivers.mk
@@ -0,0 +1,8 @@
+EMBEDDED_LINUX_DRIVERS_SLIDES = \
+    first-slide \
+    beagleboneblack-board \
+    linux-intro \
+    linux-dev-environment \
+    linux-kernel-access-hw \
+    linux-kernel-access-hw-drivers \
+    linux-kernel-access-hw-interfaces
