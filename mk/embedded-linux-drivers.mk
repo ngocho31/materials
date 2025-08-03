893a1e1f20d6ffc20d7f90ffa9025ade94323960
Ngoc Ho <phuongngoc31@gmail.com>
2025-08-03 18:39:34 +0700

    [Slides] Add linux kernel accessing hw materials
    Signed-off-by: Ngoc Ho <phuongngoc31@gmail.com>

---
+++ mk/embedded-linux-drivers.mk
@@ -0,0 +1,11 @@
+EMBEDDED_LINUX_DRIVERS_SLIDES = \
+    first-slide \
+    beagleboneblack-board \
+    linux-intro \
+    linux-dev-environment \
+    linux-kernel-access-hw \
+    linux-kernel-access-hw-drivers \
+    linux-kernel-access-hw-interfaces \
+    linux-kernel-access-hw-modules \
+    linux-kernel-access-hw-dt \
+    linux-kernel-access-hw-discovery
